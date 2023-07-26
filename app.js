57e4314e6e6c916c427f5b1e33ed10e991048dc6
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-07-26 14:55:50 -0400

    add nodemon

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+// Dummy Data 
+const SOURCE_SEED = {
+
+}
+
+const DEST_SEED = {}
+
+// Routes
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Hello, World!' });
 });
 
-const port = 3000; // You can use any preferred port number
+// Sources
+app.get('/api/sources', (req, res) => {
+  const sources = ['Source A', 'Source B', 'Source C'];
+  res.json({ sources });
+});
 
+// Destinations
+app.get('/api/destinations', (req, res) => {
+  const destinations = ['Destination X', 'Destination Y', 'Destination Z'];
+  res.json({ destinations });
+});
+
+
+// server
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
