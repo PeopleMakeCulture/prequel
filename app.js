c1ed7f5201522e55156dfe311cd55a30add7566b
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-07-26 15:54:33 -0400

    add post for sources & destinations

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 // Dummy Data - BQ
 const SOURCE_SEED = {
   "source": {
@@ -39,20 +41,39 @@ const dest_list = [DEST_SEED]
 app.get('/api', (req, res) => {
   res.status(200).json({ message: 'Hello, World!' });
 });
+app.post('/api', (req, res) => {
+  res.status(201).send('POST request to the homepage')
+});
 
 // Sources
 
-// Source - GET: returns list of sources
+// GET: returns list of sources
 app.get('/api/sources', (req, res) => {
   res.json({ sources: source_list });
 });
 
-// Source - POST: adds a new source object to source_list
+// POST: adds a new source object to source_list
+app.post('/api/sources', (req, res, next) => {
+  const newSource = req.body.source 
+  source_list.push(newSource);
+  res.status(201).json({ message: 'New source created successfully!', source: newSource});
+});
 
-// Destinations
+
+// Destinations 
+
+// GET: returns the configured data destinations
 app.get('/api/destinations', (req, res) => {
   res.json({ destinations: dest_list });
 });
+
+// POST:
+app.post('/api/destinations', (req, res, next) => {
+  const newDestination = req.body.destination;
+  dest_list.push(newDestination);
+  res.status(201).json({ message: 'New destination created successfully!', destination: newDestination });
+});
+
 
 
 // server
